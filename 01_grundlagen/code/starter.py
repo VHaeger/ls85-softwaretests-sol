fda8fe5783b88e3b2ced5a402cf50397b601942f
VHaeger <vincenth404@outlook.de>
2026-05-18 09:38:43 +0200

    [LS85] 01 Grundlagen: Aufgabe 1 gelöst

--- 01_grundlagen/code/starter.py
+++ 01_grundlagen/code/starter.py
@@ -23,13 +23,13 @@ def berechne_rabatt(preis: float, prozent: float) -> float:
 # Aufgabe 1a): Beantworte folgende Fragen als Kommentar:
 
 # Error (falsche Handlung des Entwicklers):
-# TODO: Deine Antwort hier
+# Der Entwickler vernwendet den faktor 20 statt 20%.
 
 # Defect (fehlerhafte Stelle im Code):
-# TODO: Deine Antwort hier
+# rabatt = preis * prozent  ist defekt
 
 # Failure (was der Benutzer bemerken würde):
-# TODO: Deine Antwort hier
+# Der User würde bemereken das der berchnete Preis falsch ist
 
 
 # Aufgabe 1b): Korrigiere die Funktion unten und füge print()-Tests hinzu.
@@ -39,7 +39,8 @@ def berechne_rabatt_korrigiert(preis: float, prozent: float) -> float:
     Korrigierte Version von berechne_rabatt().
     TODO: Implementiere die korrekte Logik.
     """
-    pass  # TODO: Ersetze 'pass' durch deine Implementierung
+    rabatt = prozent / 100 * preis
+    return preis - rabatt
 
 
 # Manuelle Tests (werden in Baustein 05 durch echte Unit-Tests ersetzt)
@@ -51,7 +52,10 @@ if __name__ == "__main__":
     print(berechne_rabatt(100.0, 20))  # Falsche Ausgabe erwartet
 
     print("\n=== Test: berechne_rabatt_korrigiert ===")
-    # TODO: Deine Tests hier
+    print(berechne_rabatt_korrigiert(100.0, 20))
+    print(berechne_rabatt_korrigiert(100.0, 0))
+    print(berechne_rabatt_korrigiert(100.0, 100))
+    print(berechne_rabatt_korrigiert(34.56, 50))
 
 
 # ============================================================
